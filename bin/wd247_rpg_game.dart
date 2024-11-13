25715258d5510720eeeda41ecd16c18b387e1a03
wd247 <junny3344@gmail.com>
2024-11-13 15:04:54 +0900

    기본구조

--- bin/wd247_rpg_game.dart
+++ bin/wd247_rpg_game.dart
@@ -1,3 +1,31 @@
-class Character {}
+import 'dart:io';
+import 'dart:math';
 
-void main() {}
+class Character {
+  String name;
+  int health;
+  int attack;
+  int defense;
+
+  Character(this.name, this.health, this.attack, this.defense);
+}
+
+class Monster {
+  String name;
+  int health;
+  int attack;
+  int defense = 0;
+
+  Monster(this.name, this.health, this.attack);
+}
+
+class Game {
+  void startGame() {
+    print('게임을 시작합니다!');
+  }
+}
+
+void main() {
+  var game = Game();
+  game.startGame();
+}
