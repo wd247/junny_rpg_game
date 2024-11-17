8708137efd70a8997ac2a82a249e024b16eff67e
t2024-m0156 <t2024-m0156@t2024-m0156ui-MacBookAir.local>
2024-11-17 23:37:42 +0900

    공백제거,이름짓기추가

--- bin/wd247_rpg_game.dart
+++ bin/wd247_rpg_game.dart
@@ -7,6 +7,12 @@ class Character {
   int attack;
   int defense;
 
+  void attackMonster(Monster monster) {
+    int damage = max(0, attack - monster.defense);
+    monster.health -= damage;
+    print('$name이(가) ${monster.name}에게 $damage의 데미지를 입혔습니다.');
+  }
+
   Character(this.name, this.health, this.attack, this.defense);
 }
 
@@ -20,6 +26,50 @@ class Monster {
 }
 
 class Game {
+  late Character character;
+  List<Monster> monsters = [];
+  int killedMonster = 0;
+
+  Game() {
+    loadCharacterStats();
+    // loadMonsterStats();
+  }
+
+  void loadCharacterStats() {
+    try {
+      final file =
+          File('/Users/t2024-m0156/Desktop/proj/junny_rpg_game/characters.txt');
+      final contents = file.readAsStringSync();
+      final stats = contents.split(',');
+      if (stats.length != 3) throw FormatException('Invalid character data');
+
+      int health = int.parse(stats[0]);
+      int attack = int.parse(stats[1]);
+      int defense = int.parse(stats[2]);
+
+      String name = getCharacterName();
+      character = Character(name, health, attack, defense);
+    } catch (e) {
+      print('캐릭터 데이터를 불러오는 데 실패했습니다: $e');
+      exit(1);
+    }
+  }
+
+  void loadMonstersStats() {}
+
+  String getCharacterName() {
+    while (true) {
+      stdout.write('캐릭터의 이름을 입력하세요: ');
+      String? input = stdin.readLineSync();
+      if (input != null &&
+          input.isNotEmpty &&
+          RegExp(r'^[a-zA-Z가-힣]+$').hasMatch(input)) {
+        return input;
+      }
+      print('올바르지 않은 이름입니다. 한글 또는 영문 대소문자만 사용해주세요.');
+    }
+  }
+
   void startGame() {
     print('게임을 시작합니다!');
   }
